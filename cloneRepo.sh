e9e7227433694a9af007a5f53d390b4888bcd934
Lukas Leindals <s183920@student.dtu.dk>
2020-11-09 10:16:21 +0100

    add some hpc scripts

---
+++ cloneRepo.sh
@@ -0,0 +1,9 @@
+### Download git repo
+if [ -d "../FoodRecognition" ]
+then 
+	echo "Repo already downloaded"
+else
+	git clone https://github.com/s183920/FoodRecognition.git
+
+fi
+
--- data/getData.sh
+++ data/getData.sh
@@ -1,25 +1,20 @@
 #!/bin/bash
-# curDir="{$PWD | rev | cut -d'/' -f-2 | rev}"
-# echo $curDir
 
-# if [ $curDir = FoodRecognition/data ]
-if true
-then
-	if [ -d "VOC" ] 
+if [ -f  VOC.zip ]
 	then
-		echo -e "\e[32mData already downloaded!\e[0m"
-	else
-		wget "https://drive.google.com/u/0/uc?id=1hz5AxTTVcfQz1dK0uoyfE89fMrZXQEzk&export=download"
-		mkdir VOC
-		unzip VOC.zip -d VOC
+		# original link: https://drive.google.com/file/d/1hz5AxTTVcfQz1dK0uoyfE89fMrZXQEzk/view
+		unzip VOC.zip
+		rm VOC.zip
 
-		mkdir VOC/VOCdevkit/VOC2007
-		unzip -o VOC/VOCdevkit/VOC2007.zip "VOC/VOCdevkit/VOC2007/*"
+		unzip VOC/VOCdevkit/VOC2007.zip -d VOC/VOCdevkit/
+		rm VOC/VOCdevkit/VOC2007.zip
 		
-		###If the downloaded VCTK is in tar.gz, run this:
-		#tar -xzvf VCTK-Corpus.tar.gz -C ../data/VCTK-Data
-	fi
-	
+
+elif [-d VOC ]
+	then
+		echo -e "\e[32mData already unzipped!\e[0m"
+
 else
-	echo -e "\e[31mHave you made sure to run this scripts from the 'FoodRecognition/data' directory?\e[0m"
+	# echo -e "\e[31mHave you made sure to run this scripts from the 'FoodRecognition/data' directory?\e[0m"
+	echo -e "\e[31mPlease run this script from the folder containing VOC.zip"
 fi
