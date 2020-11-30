11614ba10bca5218ef8382ff111e859821badc46
Huayuan Song <huayuan.s@gmail.com>
2020-11-30 13:37:32 +0100

    Update detect.py

--- detect.py
+++ detect.py
@@ -4,6 +4,7 @@
 Created on Thu Nov 26 00:20:09 2020
 
 @author: hso
+This .py-file follows the structure found in https://debuggercafe.com/faster-rcnn-object-detection-with-pytorch/
 """
 
 import torchvision
@@ -14,14 +15,14 @@ import cv2
 import detect_utils
 from PIL import Image
 
-# construct the argument parser
+# Construct argument parser
 parser = argparse.ArgumentParser()
 parser.add_argument('-i', '--input', help='path to input image/video')
 parser.add_argument('-m', '--min-size', dest='min_size', default=800, 
                     help='minimum input size for the FasterRCNN network')
 args = vars(parser.parse_args())
 
-# download or load the model from disk
+# Load model
 model = torchvision.models.detection.fasterrcnn_resnet50_fpn(pretrained=True, 
                                                     min_size=args['min_size'])
 
@@ -32,7 +33,7 @@ model.eval().to(device)
 boxes, classes, labels = detect_utils.predict(image, model, device, 0.8)
 image = detect_utils.draw_boxes(boxes, classes, labels, image)
 cv2.imshow('Image', image)
-save_name = f"{args['input'].split('/')[-1].split('.')[0]}_{args['min_size']}"
+save_name = "Pred_img"
 cv2.imwrite(f"outputs/{save_name}.jpg", image)
 cv2.waitKey(0)
 
