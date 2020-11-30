44fd386e03ad72300bf46269c240bf3969a37d66
Huayuan Song <huayuan.s@gmail.com>
2020-11-30 13:35:35 +0100

    Update coco_names.py

--- coco_names.py
+++ coco_names.py
@@ -5,22 +5,7 @@ Created on Thu Nov 26 00:18:04 2020
 
 @author: hso
 """
-"""
-COCO_INSTANCE_CATEGORY_NAMES = [
-    '__background__', 'person', 'bicycle', 'car', 'motorcycle', 'airplane', 'bus',
-    'train', 'truck', 'boat', 'traffic light', 'fire hydrant', 'N/A', 'stop sign',
-    'parking meter', 'bench', 'bird', 'cat', 'dog', 'horse', 'sheep', 'cow',
-    'elephant', 'bear', 'zebra', 'giraffe', 'N/A', 'backpack', 'umbrella', 'N/A', 'N/A',
-    'handbag', 'tie', 'suitcase', 'frisbee', 'skis', 'snowboard', 'sports ball',
-    'kite', 'baseball bat', 'baseball glove', 'skateboard', 'surfboard', 'tennis racket',
-    'bottle', 'N/A', 'wine glass', 'cup', 'fork', 'knife', 'spoon', 'bowl',
-    'banana', 'apple', 'sandwich', 'orange', 'broccoli', 'carrot', 'hot dog', 'pizza',
-    'donut', 'cake', 'chair', 'couch', 'potted plant', 'bed', 'N/A', 'dining table',
-    'N/A', 'N/A', 'toilet', 'N/A', 'tv', 'laptop', 'mouse', 'remote', 'keyboard', 'cell phone',
-    'microwave', 'oven', 'toaster', 'sink', 'refrigerator', 'N/A', 'book',
-    'clock', 'vase', 'scissors', 'teddy bear', 'hair drier', 'toothbrush'
-]
-"""
+
 COCO_INSTANCE_CATEGORY_NAMES = ['__background__', 'boiled peas', 'boiled potatoes', 'chopped lettuce', 'fried egg',
         'glass of milk', 'glass of water', 'meatballs', 'plain rice', 'plain spaghetti',
         'slice of bread']
